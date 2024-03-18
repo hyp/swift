8635fbc0d2c894b786286cd35475efd2ddd54ada
Alex Lorenz <arphaman@gmail.com>
2024-03-18 16:29:54 -0700

    Reapply "Driver: introduce `-sysroot` option for non-Darwin targets"
    This reverts commit abfca59247c1d3666c62c5679b43dfc40acbd4b3.

--- include/swift/AST/SearchPathOptions.h
+++ include/swift/AST/SearchPathOptions.h
@@ -368,6 +368,8 @@ private:
   std::optional<StringRef> VCToolsRoot = std::nullopt;
   std::optional<StringRef> VCToolsVersion = std::nullopt;
 
+  std::optional<StringRef> SysRoot = std::nullopt;
+
 public:
   StringRef getSDKPath() const { return SDKPath; }
 
@@ -404,6 +406,11 @@ public:
   std::optional<StringRef> getVCToolsVersion() const { return VCToolsVersion; }
   void setVCToolsVersion(StringRef version) {
     VCToolsVersion = version;
+  }
+
+  std::optional<StringRef> getSysRoot() const { return SysRoot; }
+  void setSysRoot(StringRef sysroot) {
+    SysRoot = sysroot;
   }
 
   ArrayRef<std::string> getImportSearchPaths() const {
