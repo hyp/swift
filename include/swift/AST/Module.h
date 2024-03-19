2e4d35104f7d80d0ff8d5c396dd21b809aed80e9
Alex Lorenz <arphaman@gmail.com>
2024-03-19 09:02:58 -0700

    Revert "testlibc++"
    This reverts commit da1ad3647ec334715eb3829b8129c938cf9e5f90.

--- include/swift/AST/Module.h
+++ include/swift/AST/Module.h
@@ -386,11 +386,6 @@ public:
   void setBypassResilience() { BypassResilience = true; }
 
   ArrayRef<FileUnit *> getFiles() {
-    if (Files.empty()) {
-      if (!failedToLoad()) {
-        llvm::errs() << "oops haha" << getName().str() << "\n";
-      }
-    }
     assert(!Files.empty() || failedToLoad());
     return Files;
   }
